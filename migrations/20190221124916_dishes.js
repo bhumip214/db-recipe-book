73a19535fd59a649e20f67a38314d205dc6e8930
bhumip214 <bhumip214@gmail.com>
2019-02-21 15:08:51 -0800

    Completed seeds for recipe_ingredients and instructions

--- migrations/20190221124916_dishes.js
+++ migrations/20190221124916_dishes.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("dishes", function(tbl) {
     tbl.increments();
 
-    tbl.string("name", 128).noNullable();
+    tbl.string("name", 128).notNullable();
   });
 };
 
---
+++ seeds/004-recipe_ingredients.js
@@ -0,0 +1,18 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("recipe_ingredients")
+    .truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex("recipe_ingredients").insert([
+        { quantity: "0.5", measurement:"cup", recipeId: 1, ingredientId: 1 },
+        { quantity: "1.5", measurement: "cups", recipeId: 1, ingredientId: 2},
+        { quantity: "0.5", measurement: "teaspoon", recipeId: 1, ingredientId: 3},
+        { quantity: "1.5", measurement: "cups", recipeId: 1, ingredientId: 4},
+        { quantity: "1", measurement: "teaspoon", recipeId: 1, ingredientId: 5},
+        { quantity: "0.5", measurement: "cup", recipeId: 1, ingredientId: 6},
+        { quantity: "104", measurement: "grams", recipeId: 1, ingredientId: 7 }
+        { quantity: "1", measurement: "large", recipeId: 1, ingredientId: 8}
+      ]);
+    });
+};
