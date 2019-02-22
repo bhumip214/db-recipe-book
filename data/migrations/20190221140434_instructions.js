c886b309ec99ba29c153b4ca53bf72b8776ed839
bhumip214 <bhumip214@gmail.com>
2019-02-21 16:04:10 -0800

    Created data access file and re-organized files

---
+++ data/migrations/20190221140434_instructions.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("instructions", function(tbl) {
+    tbl.increments();
+
+    tbl.integer("stepNo").notNullable();
+
+    tbl.text("step").notNullable();
+
+    tbl
+      .integer("recipeId")
+      .unsigned()
+      .references("id")
+      .inTable("recipes")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("instructions");
+};
