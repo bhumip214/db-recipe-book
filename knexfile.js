0109b443411cad8b9375ceea258c713fcb4345f4
bhumip214 <bhumip214@gmail.com>
2019-02-21 16:01:46 -0800

    Created data access file

--- knexfile.js
+++ knexfile.js
@@ -7,5 +7,11 @@ module.exports = {
       filename: "./data/recipeBook.db3"
     },
     useNullAsDefault: true
+  },
+  migrations: {
+    directory: "./data/migrations"
+  },
+  seeds: {
+    directory: "./data/seeds"
   }
 };
